437e7f5ee37a550bf57bbe3e597bd3d39c341403
cgadeau <clement.gadeau@ensae.fr>
2024-04-23 07:25:19 +0000

    hihi

--- Data_operations/Clustering/run_data_set_all.py
+++ Data_operations/Clustering/run_data_set_all.py
@@ -4,39 +4,53 @@ sys.path.append("Data_operations")
 from Clustering.new_data_set_all import *
 
 data_path = "/Users/maximecoppa/Desktop/Statapp_Data/Datas/"
-data_path_results = "/Users/maximecoppa/Desktop/Statapp_Data/Datas/"
-i = 9
+data_path_results = "/home/onyxia/Statapp-CANAL/Test/dataset"
+i = 1
 create_df_Données_Promos_odd_v2(data_path, data_path_results)
 print(i)
+i += 1
 create_df_Données_Promos_odd_all_v2(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 drop_dupplicated_columns_df_Données_Promos_v2(data_path, data_path_results, filename="df_Données_Promos_odd_v2.csv")
-print(i+2)
+print(i)
+i += 1
 create_df_Données_Reabos_odd_all_v2(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 enlever_abos(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 enlever_nan(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 ajout_delai(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_n_reabos_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_delai_reabo_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_diff_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_diff_p_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_n_month_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_n_month_pourc_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 create_new_data_set_n_fidelite_v3(data_path, data_path_results)
-print(i+2)
+print(i)
+i += 1
 
-print(i+2)
 huge_data_set(data_path, data_path_results)
-print(i+2)
+print(i)
